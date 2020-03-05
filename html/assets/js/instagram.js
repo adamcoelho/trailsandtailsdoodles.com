9cf58a01f786160cd6771244a9505230f01d1d4c
Christian Samuel <christian.leumas@icloud.com>
2020-03-04 20:16:06 -0800

    Setup instagram gallery
    - Added javascript call to s3 to get instagram feed links and populate gallery
    - Adjusted the gallery card-img class fix height issues for images that were taken in portrait

---
+++ html/assets/js/instagram.js
@@ -0,0 +1,14 @@
+let Http = new XMLHttpRequest();
+let url ='https://s3.us-east-2.amazonaws.com/instagram.trailsandtailsdoodles.com/instagram_feed.json';
+Http.open("GET", url);
+Http.send();
+
+Http.onreadystatechange = function() {
+  if(this.readyState === 4 && this.status === 200) {
+    let json = JSON.parse(Http.responseText);
+
+    for (let i = 0; i < 16; i++) {
+      document.getElementById("img" + i).src = json[i].media_url;
+    }
+  }
+};
