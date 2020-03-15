5f18f4d43dc11377c683cb340c95914808ef9b3d
Christian Samuel <christian.leumas@icloud.com>
2020-03-15 12:39:09 -0700

    Add links to thumbnails based on payload length

--- html/assets/js/instagram.js
+++ html/assets/js/instagram.js
@@ -6,8 +6,7 @@ Http.send();
 Http.onreadystatechange = function() {
   if(this.readyState === 4 && this.status === 200) {
     let json = JSON.parse(Http.responseText);
-
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < json.length; i++) {
       document.getElementById("img" + i).src = json[i].media_url;
     }
   }
